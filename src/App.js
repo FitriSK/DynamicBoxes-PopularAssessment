4bd3cbb59f260df720659c147e789f0143a18e9a
FitriSK <nazarul@sitikhadijah.com>
2021-11-18 01:36:08 +0800

    first commit upon completion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState();
+  const boxes = [];
+
+  let idx = 1;
+  for (let i = 1; i <= number; i++) {
+    idx = idx * 2;
+    let wdx = idx * 10 + "px";
+    boxes.push(<div style={{"width": wdx, "height": wdx}} id="box"></div>);
+  }
+
+  /*
+1*1=1 | 2*2=4 | 3*3=9
+1*2=2 | 2*2=4 | 3*2=6
+1+1=2 | 2+2=4 | 3+3=6
+1+2=3 | 2+2=4 | 3+2=5
+
+1*2=2 | 2*2=4 | 4*2=8 | 8*2=16 | 16*2=32
+2*2=4 | 4*2=8 | 8*2=16
+
+1 2 4
+2 4 8
+3 6 12
+  */
+
+  function handleChange(event) {
+    setNumber(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Dynamic Boxes</h1>
+      <input onChange={handleChange} />
+      <div className="boxArea">{boxes}</div>
     </div>
   );
 }
